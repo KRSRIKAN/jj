b7987aca0a389621e6965cb7e7281b0b58122067
srikanth <kristsrikanth@gmail.com>
2022-04-06 17:56:41 +0530

    first commit

---
+++ k1/src/com/cap/k1/mynewworld.java
@@ -0,0 +1,10 @@
+package com.cap.k1;
+
+public class mynewworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("");
+	}
+
+}
---
+++ k1/src/com/cap/k1/package-info.java
@@ -0,0 +1,1 @@
+package com.cap.k1;
